b444561765d7c144fe8a383e6fcb25a56df2faf1
Eléa De Sousa <bitsch.elea@gmail.com>
2026-05-31 09:50:07 +0200

    Add character constraints to password validation
    Closes #41

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -14,6 +14,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class RegistrationFormType extends AbstractType
 {
@@ -39,6 +40,10 @@ class RegistrationFormType extends AbstractType
                 'constraints' => [
                     new NotBlank(),
                     new Length(min: 8, max: 4096),
+                    new Regex(
+                        pattern: '/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).+$/',
+                        message: 'Le mot de passe doit contenir au moins une majuscule, une minuscule, un chiffre et un caractère spécial.',
+                    ),
                 ],
             ])
             ->add('agreeTerms', CheckboxType::class, [
